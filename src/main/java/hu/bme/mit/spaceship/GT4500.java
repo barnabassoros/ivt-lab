6b0e9044a75df80992cd054d03659b9476c9bde9
barnabassoros <soros14@gmail.com>
2022-04-01 11:08:33 +0200

    fixed test

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -78,7 +78,18 @@ public class GT4500 implements SpaceShip {
 
       case ALL:
         // try to fire both of the torpedo stores
-        //TODO implement feature
+        boolean firstfiringSuccess = false;
+        boolean secondfiringSuccess = false;
+        if(!primaryTorpedoStore.isEmpty()) {
+          firstfiringSuccess = primaryTorpedoStore.fire(1);
+        }
+        if(!secondaryTorpedoStore.isEmpty()) {
+          secondfiringSuccess = secondaryTorpedoStore.fire(1);
+        }
+
+
+        firingSuccess = (firstfiringSuccess && secondfiringSuccess);
+
 
         break;
     }
